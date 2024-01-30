7479f94a85b90e025896736b160f926f6dbade12
zerohost7375@gmail.com <zerohost7375@gmail.com>
2024-01-31 00:15:24 +0900

    컨트롤러 작성

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -1,15 +1,34 @@
 package com.codingrecipe.board.controller;
 
+import com.codingrecipe.board.dto.BoardDto;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+    @Autowired
+    BoardService boardService;
+
     // 버튼을 누르든 링크를 타든 요청방식은 GET
     @GetMapping("/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDto boardDto) {
+        System.out.println("boardDto = " + boardDto);
+        boardService.save(boardDto);
+
+        return "index";
+
     }
 }
--- src/main/java/com/codingrecipe/board/controller/HomeController.java
+++ src/main/java/com/codingrecipe/board/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.codingrecipe.board.controller;
 
 
+import com.codingrecipe.board.dto.BoardDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -11,4 +16,6 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+
 }
