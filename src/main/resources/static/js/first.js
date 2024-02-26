c03091f08470d6c512f9cc71147ea867768ad992
zerohost7375@gmail.com <zerohost7375@gmail.com>
2024-02-26 22:51:09 +0900

    수정함

---
+++ src/main/resources/static/js/first.js
@@ -0,0 +1,16 @@
+// index.html JS
+
+
+// function saveRe(){}
+// 위의 거랑 같은거임
+const saveReq = () => {
+    location.href = "/board/save";
+}
+
+// const listReq = () => {
+//     location.href= "/board/"
+// }
+
+const pagingReq = () => {
+    location.href = "/board/paging";
+}
